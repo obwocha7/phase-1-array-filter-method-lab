744143109d3b62f17ec66b165762b3f62b886aea
Derrick <omoriderrick@gmail.com>
2024-04-03 13:03:58 +0300

    New Feat: array.prototype.filter

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 // Code your solution here
+// `findMatching`- This function takes an array of drivers' names and a `string`
+//   as arguments, and returns the matching list of drivers. The function should be
+//   case insensitive.
+const drivers = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function findMatching(arr, string) {
+  const driver = [];
+  for (let i of arr) {
+    if (i.toLowerCase() === string.toLowerCase()) {
+      driver.push(i);
+    }
+  }
+  return driver;
+}
+
+function fuzzyMatch(arr, string) {
+  const driver = [];
+  for (let i of arr) {
+    if (i.toLowerCase()[0] === string.toLowerCase()[0]) {
+      driver.push(i);
+    }
+  }
+  return driver;
+}
+
+function matchName(arr, string) {
+  const driver = [];
+  for (let i of arr) {
+    if (i.name.toLowerCase() === string.toLowerCase()) {
+      driver.push(i);
+    }
+  }
+  return driver;
+}
+
+matchName(drivers, "bobby");
